1c77ba55f5d95ba4bc104c254e60571bc36a0222
FED <64375006+fedev-rgb@users.noreply.github.com>
2024-06-17 09:31:20 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import {
   Field, CellGroup, Button, Search,
 } from 'vant';
@@ -8,7 +9,9 @@ import './assets/main.css';
 import App from './App.vue';
 
 const app = createApp(App);
+const pinia = createPinia();
 
+app.use(pinia);
 app.use(Field);
 app.use(CellGroup);
 app.use(Button);
